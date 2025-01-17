03ca4331a2d685e98516cc20f7cb6b713b9c20ac
Abhimanyu Gupta <120638939+abhimanyu345@users.noreply.github.com>
2025-01-17 19:36:49 +0530

    Add files via upload

---
+++ POTD LEETCODE/Jan/Day17-2683.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int ans = 0;
+        for(int i=0; i<derived.size(); i++){
+            ans ^= derived[i];
+        }
+        if(!ans){
+            return true;
+        }
+        return false;
+    }
+};
